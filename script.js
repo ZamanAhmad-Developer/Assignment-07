af3db7714cce6af4f5c86fdb64a743369f39c066
ZamanAhmad-Developer <zamanahmad0305@gmail.com>
2023-03-30 13:18:17 -0400

    first commit

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 //var Number1 =+  prompt("Enter the First Number")
 //var Number2 =+ prompt("Enter the Second Number")
 //if(  Number1 >  Number2){
- //   console.log(" The Number is Maximum");
+//    console.log(" The Number1 is Maximum");
 //}
-//else{
-//    console.log(" The Number is Minimum");
+//else if (Number2 > Number1) {
+//    console.log(" The Number2 is Minimum");
 //}
 
 
 //-------------------------------------------------------------------//
+
+//2. Write a js program to find maximum between three numbers.
+
+//var  Number1 =+ prompt("Enter the First Number")
+//var  Number2 =+ prompt("Enter the Second Number")
+//var  Number3 =+ prompt("Enter the Third Number")
+//if( Number1 > Number2 && Number1 > Number3){
+  //  console.log(  "First Number is maximum")
+//}
+//else if( Number3 > Number1 && Number3 > Number2){
+  //  console.log(  "Third Number is Maximum")
+//}
+//else{ console.log(  "Second Number is Maximum")}
+
+//------------------------------------------------------------------------------//
