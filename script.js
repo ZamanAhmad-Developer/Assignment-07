116e5ed5b6ced1961febe81ad506507ea4a0ed8f
ZamanAhmad-Developer <zamanahmad0305@gmail.com>
2023-03-30 12:37:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1. Write a js program to find maximum between two numbers.
+//var Number1 =+  prompt("Enter the First Number")
+//var Number2 =+ prompt("Enter the Second Number")
+//if(  Number1 >  Number2){
+ //   console.log(" The Number is Maximum");
+//}
+//else{
+//    console.log(" The Number is Minimum");
+//}
+
+
+//-------------------------------------------------------------------//
